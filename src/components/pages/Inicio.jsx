767da44e0816185926885de93f19f4592d5db885
Maxi Ordoñez <exemaxi@outlook.com>
2025-08-29 20:37:20 -0300

    se agrego termino el maquetado del inicio, se agrego un carousel, titulo, imput.

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -1,15 +1,33 @@
 import CarouselInicio from "./CarouselInicio";
-
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 const Inicio = () => {
-    return (
-        <>
-        <CarouselInicio />
+  return (
+    <>
+      <CarouselInicio />
+      <div className="my-3 bord-top">
+        <h1>Nuestros productos</h1>
         <div>
-            <h1>Nuestros productos</h1>
+          <Form>
+            <Form.Group className="mb-3 d-flex" controlId="formBasicEmail">
+              <Form.Control
+                type="text"
+                placeholder="Ingresa el nombre del producto."
+                className="me-2" 
+              />
+              <Button variant="primary" type="submit">
+                Buscar
+              </Button>
+            </Form.Group>
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form>
         </div>
-        </>
-    );
+      </div>
+    </>
+  );
 };
 
 export default Inicio;
