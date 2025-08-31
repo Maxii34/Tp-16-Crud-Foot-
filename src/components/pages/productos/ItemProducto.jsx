4610af4e48df9a6d31a2924c5ca6457101b13cde
Maxi Ordoñez <exemaxi@outlook.com>
2025-08-30 23:02:40 -0300

    Las tablas quedaron responcive, textos centrados y imagen centrada y con efecto.

--- src/components/pages/productos/ItemProducto.jsx
+++ src/components/pages/productos/ItemProducto.jsx
@@ -5,7 +5,7 @@ const ItemProducto = () => {
     <div className="table-responsive">
       <Table striped bordered hover>
         <thead>
-          <tr>
+          <tr class="text-center align-middle">
             <th>#</th>
             <th>Producto</th>
             <th>Precio</th>
@@ -15,29 +15,24 @@ const ItemProducto = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
+          <tr class="text-center align-middle">
             <td>1</td>
             <td>Pizza margarita clasica</td>
             <td>$ 4.500</td>
-            <td>Imagen</td>
+            <td>
+              <img src="https://images.pexels.com/photos/28509915/pexels-photo-28509915.jpeg" class="img-fluid rounded table-img" alt="Pizza"></img>
+            </td>
             <td>Categoria</td>
             <td>
               <div class="d-flex justify-content-center gap-2">
-                <button class="btn btn-primary"><i class="bi bi-pencil-square"></i></button>
-                <button class="btn btn-danger"><i class="bi bi-trash"></i></button>
+                <button class="btn btn-warning btn-sm shadow">
+                  <i class="bi bi-pencil-square"></i>
+                </button>
+                <button class="btn btn-danger btn-sm shadow">
+                  <i class="bi bi-trash"></i>
+                </button>
               </div>
             </td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td colSpan={2}>Larry the Bird</td>
-            <td>@twitter</td>
           </tr>
         </tbody>
       </Table>
