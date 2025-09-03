5b3e392664f72ddbd4426c02cc1fbe303672ba0f
Maxi Ordoñez <exemaxi@outlook.com>
2025-09-03 20:25:12 -0300

    Se agregaron las validaciones al formulario de formularioProductos

--- src/components/pages/productos/FormularioProductos.jsx
+++ src/components/pages/productos/FormularioProductos.jsx
@@ -1,8 +1,17 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router";
 
 const FormularioProductos = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    setValue,
+    formState: { errors },
+  } = useForm();
+
   return (
     <section className=" container my-2">
       <div className="d-flex align-items-center justify-content-between mb-3 border-bottom py-2">
@@ -18,17 +27,45 @@ const FormularioProductos = () => {
           <Form.Control
             type="text"
             placeholder="Ingresa el nombre del producto"
-            name="nombre"
+            {...register("nombreProducto", {
+              required: "El nombre del producto es un dato obligatorio",
+              minLength: {
+                value: 2,
+                message:
+                  "El nombre del producto debe tener almenos 2 caracteres",
+              },
+              maxLength: {
+                value: 100,
+                message:
+                  "El nombre del producto debe tener como maximo 100 caracteres",
+              },
+            })}
           />
+          <Form.Text className="text-danger">
+            {errors.nombreProducto?.message}
+          </Form.Text>
         </Form.Group>
 
         <Form.Group className="mb-3">
           <Form.Label>Precio</Form.Label>
           <Form.Control
             type="number"
-            placeholder="Ingresa el precio"
-            name="precio"
+            placeholder="Agrega el precio"
+            {...register("precio", {
+              required: "El precio es un campo obligatorio",
+              minLength: {
+                value: 1,
+                message: "El precio debe tener al menos 1 dígito",
+              },
+              maxLength: {
+                value: 6,
+                message: "El precio no puede superar los 6 dígitos",
+              },
+            })}
           />
+          <Form.Text className="text-danger">
+            {errors.precio?.message}
+          </Form.Text>
         </Form.Group>
 
         <Form.Group className="mb-3">
@@ -36,8 +73,21 @@ const FormularioProductos = () => {
           <Form.Control
             type="url"
             placeholder="https://ejemplo.com/imagen.jpg"
-            name="imagenUrl"
+            {...register("imagenUrl", {
+              required: "La URL de la imagen es obligatoria",
+              minLength: {
+                value: 10,
+                message: "La URL debe tener al menos 10 caracteres",
+              },
+              maxLength: {
+                value: 200,
+                message: "La URL no puede superar los 200 caracteres",
+              },
+            })}
           />
+          <Form.Text className="text-danger">
+            {errors.imagenUrl?.message}
+          </Form.Text>
         </Form.Group>
 
         <Form.Group className="mb-3">
@@ -45,7 +95,9 @@ const FormularioProductos = () => {
           <Form.Select
             name="categoria"
             aria-label="Selecciona una categoría"
-            required
+            {...register("categoria", {
+              required: "Debe seleccionar una categoria",
+            })}
           >
             <option value="">Selecciona una categoría...</option>
             <option value="electronica">Electrónica</option>
@@ -62,10 +114,21 @@ const FormularioProductos = () => {
             as="textarea"
             rows={2}
             placeholder="Describe el producto (características, detalles, etc.)"
-            name="descripcion"
-            maxLength={500}
+            {...register("descripcionBreve", {
+              required: "La descripción breve es obligatoria",
+              minLength: {
+                value: 10,
+                message: "La descripción debe tener al menos 10 caracteres",
+              },
+              maxLength: {
+                value: 300,
+                message: "La descripción no puede superar los 300 caracteres",
+              },
+            })}
           />
-          <Form.Text muted>Máximo 300 caracteres.</Form.Text>
+          <Form.Text className="text-danger">
+            {errors.descripcionBreve?.message}
+          </Form.Text>
         </Form.Group>
 
         <Form.Group className="mb-3">
@@ -74,12 +137,24 @@ const FormularioProductos = () => {
             as="textarea"
             rows={3}
             placeholder="Describe el producto (características, detalles, etc.)"
-            name="descripcion"
+            {...register("descripcionAmplia", {
+              required: "La descripción amplia es obligatoria",
+              minLength: {
+                value: 20,
+                message: "La descripción debe tener al menos 20 caracteres",
+              },
+              maxLength: {
+                value: 1000,
+                message: "La descripción no puede superar los 1000 caracteres",
+              },
+            })}
           />
-          <Form.Text muted>Máximo 500 caracteres.</Form.Text>
+          <Form.Text className="text-danger">
+            {errors.descripcionAmplia?.message}
+          </Form.Text>
         </Form.Group>
 
-        <Button variant="success" type="submit" className=" shadow">
+        <Button variant="success" type="submit" className="shadow">
           Guardar Producto
         </Button>
       </Form>
